b03b0b7441aa90211f7e6de51fa5d4e7c478d1cc
Itzfeminisce <rofesol.ng@gmail.com>
2023-01-01 16:50:22 -1100

    contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,31 +4,49 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $nav = $("#nav");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: [null, "736px"],
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Scrolly.
+  $("#nav a, .scrolly").scrolly({
+    speed: 1000,
+    offset: function () {
+      return $nav.height();
+    },
+  });
 
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
+  $("form[name='contact']").on("submit", function (event) {
+    event.preventDefault();
+    const form = $(this);
+    const inputs = $(this).find("input");
+    for (const input of inputs) {
+      if ($(input).val().trim().length <= 0) {
+        alert(`Field [${$(input).attr("name")}] is required.`);
+        return false;
+      }
+    }
 
+    window.open(
+      `mailto:test@example.com?subject=${$(form)
+        .find("#subject")
+        .val()}/&body=${$(form).find("#message").val()}`
+    );
+  });
 })(jQuery);
